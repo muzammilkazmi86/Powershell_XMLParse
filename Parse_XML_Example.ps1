790e0e10345c074bbe39059a194d34c49a39a79e
Syed Kazmi <35359630+muzammilkazmi86@users.noreply.github.com>
2022-01-17 10:36:38 -0600

    Add files via upload

---
+++ Parse_XML_Example.ps1
@@ -0,0 +1,20 @@
+﻿#Casting XML Strings to Objects
+[xml]$xmlObj_books = Get-Content -Path .\books.xml
+
+#List all books
+$xmlObj_books.catalog.book
+
+# List Book with ID bk111
+$ID_result = $xmlObj_books.catalog.book | where id -EQ "bk111"
+
+# Fetch only author information from book with ID bk111
+$ID_result.author
+
+# Fetch all Book titles
+$xmlObj_books.catalog.book | Select-Object title
+
+
+
+# Parsing Powershell XML Elements with Select-Xml
+# Get all book name titles
+Select-Xml -Path books.xml -XPath '/catalog/book/title' | ForEach-Object { $_.Node.InnerXML }
